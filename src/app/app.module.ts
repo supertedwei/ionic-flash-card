c796c985d7ab31b626b87a3dc9784e3e5a829488
Ted Wei <jdwei@hotmail.com>
2017-02-08 21:31:50 +0800

    example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { FlashCardComponent } from '../components/flash-card/flash-card';
+ 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FlashCardComponent
   ],
   imports: [
     IonicModule.forRoot(MyApp)
--- src/components/flash-card/flash-card.ts
+++ src/components/flash-card/flash-card.ts
@@ -1,22 +1,19 @@
 import { Component } from '@angular/core';
-
-/*
-  Generated class for the FlashCard component.
-
-  See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
-  for more info on Angular 2 Components.
-*/
+ 
 @Component({
   selector: 'flash-card',
   templateUrl: 'flash-card.html'
 })
 export class FlashCardComponent {
-
-  text: string;
-
+ 
+  flipped: boolean = false;
+ 
   constructor() {
-    console.log('Hello FlashCard Component');
-    this.text = 'Hello World';
+ 
   }
-
+ 
+  flip(){
+    this.flipped = !this.flipped;
+  }
+ 
 }
